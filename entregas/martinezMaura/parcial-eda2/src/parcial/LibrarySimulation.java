5dce28f5ec822715205d08ad4310fe3484a4bd93
Martinez-rosa <maura.martinez@alumnos.uneatlantico.es>
2024-04-10 14:25:14 +0200

    Update

--- entregas/martinezMaura/parcial-eda2/src/parcial/LibrarySimulation.java
+++ entregas/martinezMaura/parcial-eda2/src/parcial/LibrarySimulation.java
@@ -3,7 +3,6 @@ package parcial;
 public class LibrarySimulation {
 
     public static void main(String[] args) {
-        LibraryManager libraryManager = new LibraryManager();
-        libraryManager.displayMenu();
+        new LibraryManager().displayMenu();
     }
 }
